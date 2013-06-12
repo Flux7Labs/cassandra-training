fd9bd32db26277a47de85544e4f60de9378b0f1d
Rizwan Sharif <rizwan6532@gmail.com>
2013-06-11 21:23:54 -0700

    hector cql example

--- hector/src/main/java/com/flux7/HectorCQL.java
+++ hector/src/main/java/com/flux7/HectorCQL.java
@@ -2,21 +2,37 @@ package com.flux7;
 
 import me.prettyprint.cassandra.model.CqlQuery;
 import me.prettyprint.cassandra.model.CqlRows;
-import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.beans.HColumn;
+import me.prettyprint.hector.api.beans.Row;
+import me.prettyprint.hector.api.factory.HFactory;
 import me.prettyprint.hector.api.query.QueryResult;
 
-public class HectorCQL {
+public class HectorCQL extends CassandraConfig {
 
-	private final static String KEYSPACE = "User";
-	private static final StringSerializer se = new StringSerializer();
-	private static final LongSerializer le = new LongSerializer();
-
-	public static void main(String[] args) {
-		CqlQuery<String,String,Long> cqlQuery = new CqlQuery<String,String,Long>(keyspace, se, se, le);
-	    cqlQuery.setQuery("select * from User");
-	    QueryResult<CqlRows<String,String,Long>> result = cqlQuery.execute();
-	    CqlRows<String, String, Long> rows = result.get();
+	public static void main(String[] args) throws Exception {
+		
+		Cluster cluster = HFactory.getOrCreateCluster("test", CASSANDRA_ADDRESS);
+		 
+		StringSerializer se = new StringSerializer();
+		
+		Keyspace keyspace = HFactory.createKeyspace("test", cluster);
+	    
+		CqlQuery<String, String, String> cqlQuery = new CqlQuery<String,String,String>(keyspace, se, se, se);
+		
+	    cqlQuery.setQuery("select * from users");
+	    QueryResult<CqlRows<String,String,String>> result = cqlQuery.execute();
+	    CqlRows<String, String, String> rows = result.get();
+	    
+	    for (Row<String, String, String> row : rows){
+	    	for(HColumn column :row.getColumnSlice().getColumns()){
+	    		System.out.println(column.getName()+" :: "+ column.getValue());
+	    		
+	    	}
+	    	System.out.println("------------");
+	    }
 	}
 
 }
